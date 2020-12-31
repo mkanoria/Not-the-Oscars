0c16290c1497812c72f11e7c15c1013dd937d0bd
Mayank Kanoria <mayankkanoria@gmail.com>
2020-12-30 19:15:08 -0500

    Added default list

---
+++ src/topMovies.js
@@ -0,0 +1,44 @@
+export const myTopMovies = [
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+    Year: "2019",
+  },
+  {
+    imdbID: "tt7939766",
+    Title: "I'm Thinking of Ending Things",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNWMyZTA1MTItMzFhOS00NGY5LWJlZDMtMzczZmRjOThkMmViXkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_SX300.jpg",
+    Year: "2020",
+  },
+  {
+    imdbID: "tt8579674",
+    Title: "1917",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SX300.jpg",
+    Year: "2019",
+  },
+  {
+    imdbID: "tt7784604",
+    Title: "Hereditary",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_SX300.jpg",
+    Year: "2018",
+  },
+  {
+    imdbID: "tt8946378",
+    Title: "Knives Out",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMGUwZjliMTAtNzAxZi00MWNiLWE2NzgtZGUxMGQxZjhhNDRiXkEyXkFqcGdeQXVyNjU1NzU3MzE@._V1_SX300.jpg",
+    Year: "2019",
+  },
+  {
+    imdbID: "tt2582802",
+    Title: "Whiplash",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTA5NDZlZGUtMjAxOS00YTRkLTkwYmMtYWQ0NWEwZDZiNjEzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    Year: "2014",
+  },
+];
