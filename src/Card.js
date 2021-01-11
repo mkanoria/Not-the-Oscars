491232fc41a48d3d1bfbf038d0d6494e74375fe3
Mayank Kanoria <mayankkanoria@gmail.com>
2021-01-10 19:31:04 -0500

    Reorder imports

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
-
 import Card from "@material-ui/core/Card";
-import Grid from "@material-ui/core/Grid";
-import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import InfoIcon from "@material-ui/icons/Info";
-
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,10 +27,14 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     flex: "1 0 auto",
+    width: 140,
     // flexGrow: 1,
   },
   cover: {
     width: 151,
+    maxWidth: "100%",
+    // maxHeight: "100%",
+    objectFit: "contain",
     marginLeft: "auto",
     // width: "100%",
     // height: 0,
